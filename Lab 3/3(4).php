b9d481099d66292e7b226b0977f2cf36a39734d5
Omarfarhanabid <90982254+Omarfarhanabid@users.noreply.github.com>
2021-10-23 21:10:17 +0600

    Add files via upload

---
+++ Lab 3/3(4).php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:<br><br>
+<img src="th.jpg" width="150" height="160">
+<br><br>
+  <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
