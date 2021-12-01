e6264957a61f7fb01ef8fe705c1c9f339a2acab6
Omarfarhanabid <90982254+Omarfarhanabid@users.noreply.github.com>
2021-12-02 01:34:51 +0600

    lab 5

---
+++ lab 5/facultylog.php
@@ -0,0 +1,56 @@
+
+
+<?php 
+
+session_start();
+
+$UserID="19-39767-1";
+$password="12345678";
+
+if (isset($_POST['uname']))
+
+
+ {
+  if ($_POST['uname']==$UserID && $_POST['pass']==$password) {
+    $_SESSION['uname'] = $UserID;
+    header("location:faclogfirst.php");
+  }
+  else{
+    $msg="UserID or password invalid";
+    // echo "<script>alert('uname or pass incorrect!')</script>";
+  }
+
+}
+
+ ?>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+
+  <p style="color: red;"> *UserId and password required</p>
+  <table align="center">
+    
+    <tr>
+      <th colspan="2"><h2>Login---Enter your ID & Password</h2></th>
+    </tr>
+    <?php if(isset($msg)){?>
+        <tr>
+          <td colspan="2" align="center" valign="top"><?php echo $msg;?></td>
+        </tr>
+      <?php } ?>
+    <tr>
+      <td>UserID</td>
+      <td><input type="text" name="uname"></td>
+    </tr>
+    <tr>
+      <td>Password</td>
+      <td><input type="password" name="pass"></td>
+    </tr>
+    <input type="checkbox" name="remember" /> Remember me  
+    <tr>
+      <td align="right" colspan="2"><input type="submit" name="login" value="login"></td>
+    </tr>
+  </table>
+  
+</form>
+<a href="facultyforgot.php">Forgot Password?</a><br><br>
+<button><a href="HomePage.php">Go To HomePage </button>
---
+++ lab 5/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>  
+<head>
+<title>login</title>
+
+</head>
+<h1 align='center'>WELCOME TO AIUB PORTAL</h1>
+<h2 align="center">What type of user are you!</h2>
+
+<button><a href="adminlogin.php">  Admin.................</button><br><br>
+
+
+
+<button><a href="facultylog.php">  Faculty.................. </button><br><br>
+
+
+
+<button><a href="studentlogin.php">  Student................... </button><br><br>
+
+
+<br>
+<br>
+<button><a href="workerlogin.php">  Worker.................... </button><br><br>
+<button><a href="HomePage.php">Go To HomePage </button>
+</tr>
+<tr>
+</tr></b>
+</table>
+</html>
