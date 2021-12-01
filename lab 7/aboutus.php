bd435d96062f52bc2ff24f7f23b459b0e6629c40
Omarfarhanabid <90982254+Omarfarhanabid@users.noreply.github.com>
2021-12-02 01:38:33 +0600

    lab 7

---
+++ lab 7/aboutus.php
@@ -0,0 +1,39 @@
+
+
+<html>
+
+
+<head>
+	<link rel="stylesheet" type="text/css" media="screen and (max-width: 600px)" href="mobile.css">
+<link rel="stylesheet" type="text/css" href="homepage.css">
+</head>
+
+<body>
+
+
+<div class="about">
+<h1><a href="General.php">General Information</a><br></a></h1> 
+<br>
+<br>
+<br>
+
+
+
+
+<h1><a href="Why.php">Why Study at UE</a><br></a></h1> 
+
+<br><br><br>
+
+<h1><a href="Rules.php">Rule_Of_Entry</a><br></a></h1> 
+
+
+<br><br><br>
+
+
+<h1><a href="Resource.php">Resource</a><br></a></h1> 
+<br><br><br><br><br>
+
+</div>
+<button><a href="HomePage.php">Go To HomePage </button>
+</body>
+</html>
